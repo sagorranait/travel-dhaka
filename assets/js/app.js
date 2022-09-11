8c0f22a7099c3f8fdf2bb2ba6aeb43d6fdb6314e
SAGOR RANA <60606778+sagorranait@users.noreply.github.com>
2022-09-11 11:53:27 +0600

    Added Background-Image to Popular-Places section.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
 // Header popular places to discover slider
+$('#popularPlaces').owlCarousel({
+  loop:true,
+  autoplay:true,
+  autoplayTimeout: 3500,
+  autoplayHoverPause:true,
+  margin: 30,
+  nav:false,
+  dots: false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
